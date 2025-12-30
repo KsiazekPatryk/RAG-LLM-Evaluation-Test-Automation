6bb032165c73685d098af3bbdaea56bca619fbec
Patryk Ksiazek <globalghost1@protonmail.com>
2025-12-30 20:50:19 +0100

    Optimize LLM Evaluation

--- Test3_Framework.py
+++ Test3_Framework.py
@@ -9,25 +9,39 @@ import requests
 os.environ["OPENAI_API_KEY"] = "your_openai_api_key_here"
 
 @pytest.mark.asyncio
-async def test_context_recall():
-    llm = ChatOpenAI(model="gpt-4", temperature=0)
-    lang_chain_llm = LangchainLLMWrapper(llm)
-    context_recall = LLMContextRecall(llm=lang_chain_llm)
+@pytest.mark.parametrize("getData", 
+                         [
+                             {
+                                 "question" : "How many articles are there in the Selenium webdriver python course?",
+                                 "reference" : "23"
+                             }
+                         ],indirect=True
+)
+async def test_context_recall(llm_wrapper,getData):
     question = "How many articles are there in the Selenium webdriver python course?"
+
+    context_recall = LLMContextRecall(llm=llm_wrapper)
+    score = await context_recall.single_turn_ascore(getData)
+    print(score)
+    assert score > 0.7
+
+   
+@pytest.fixture
+def getData(request):
+    test_data = request.param
+
     responseDict = requests.post("https://rahulshettyacademy.com/rag-llm/ask", 
                   json={
-                        "question": question,
+                        "question": test_data["question"],
                         "chat_history": [                           
                         ]
                     }).json()
     
     sample = SingleTurnSample(
-        user_input="How many articles are there in the Selenium webdriver python course?",
+        user_input=test_data["question"],
         retrieved_contexts=[responseDict["retrieved_docs"][0]["page_content"],
                             responseDict["retrieved_docs"][1]["page_content"],
                             responseDict["retrieved_docs"][2]["page_content"]],
-        reference="23"
+        reference=test_data["reference"],
     )
-    score = await context_recall.single_turn_ascore(sample)
-    print(score)
-    assert score > 0.7
+    return sample
---
+++ conftest.py
@@ -0,0 +1,9 @@
+import pytest
+from langchain_openai import ChatOpenAI
+from ragas.llms import LangchainLLMWrapper
+
+@pytest.fixture
+def llm_wrapper():
+        llm = ChatOpenAI(model="gpt-4", temperature=0)
+        lang_chain_llm = LangchainLLMWrapper(llm)
+        return lang_chain_llm
