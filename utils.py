1731438a67de5eb1a9c893a2dc9b65b5bcecdf21
Patryk Ksiazek <globalghost1@protonmail.com>
2026-01-02 21:26:38 +0100

    Changes

--- utils.py
+++ utils.py
@@ -18,3 +18,20 @@ def get_llm_response(test_data):
                         ]
                     }).json()
     return responseDict
+
+
+# Generate Rubrics based Criteria Scoring to evaluate LLM Response. This metric that is used to evaluate response. The rubric consists of descriptions for each score, typically
+# ranging from 1 to 10. The response here is evalutaion based on score_descriptions and ground truth.
+
+#rubrics = {
+# "score1_description": The response is incorrect, irrelevant, or does not align with the ground truth.",
+# "score2_description": The response partially matches the ground truth but includes significant errors,omissions, or irrelevant information."
+# "score3_description": The response generally aligns with the ground truth but may lack detail, clarity or have mino inaccuracies."
+# "score4_description": The response is mostly accurate and aligns well with the ground truth, with  only minor issues or missing details"
+# "score5_description": The response is full accurate, aligns completely with the ground truth,and is clear and detailed.
+
+#}
+
+#user_input= "Where is the Eiffel Tower located?"
+#response = "The Eiffel Tower is located in Europe and it is part of France"
+#reference= "The Eiffel Tower is located in Paris"
